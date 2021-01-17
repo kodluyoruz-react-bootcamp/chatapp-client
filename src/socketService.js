bdb373d0262816aedb2c819dc6fa3452b6412791
Mehmet Seven <mehmetseven0@gmail.com>
2021-01-17 18:57:33 +0300

    backend endpoint

--- src/socketService.js
+++ src/socketService.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 let socket;
 
 export const initSocket = () => {
-	socket = io('https://chatkodluyoruz.rocknjs.com/', {
+	socket = io('https://kodluyoruz.rocknjs.com/', {
 		transports: ['websocket'],
 	});
 
